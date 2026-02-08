d7491ef77c59e5d53b333457482a9a101cc79dc5
HBarrera25 <00226325@uca.edu.sv>
2026-02-07 21:20:11 -0600

    fix: errores

--- registro_estudiantes.cpp
+++ registro_estudiantes.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 using namespace std;
 
-struct Estudiante {
+struct Estudiante
+{
     int carnet;
     char nombre[50];
     float nota;
-    Estudiante* izquierdo;
-    Estudiante* derecho;
+    Estudiante *izquierdo;
+    Estudiante *derecho;
 };
 
 // Función para crear un nuevo estudiante
-Estudiante* crearEstudiante(int carnet, char nom[], float nota) {
-    Estudiante* nuevo = new Estudiante();
+Estudiante *crearEstudiante(int carnet, char nom[], float nota)
+{
+    Estudiante *nuevo = new Estudiante();
     nuevo->carnet = carnet;
     nuevo->nota = nota;
     nuevo->izquierdo = NULL;
     nuevo->derecho = NULL;
 
     int i = 0;
-    while (nom[i] != '\0' && i < 49) {
+    while (nom[i] != '\0' && i < 49)
+    {
         nuevo->nombre[i] = nom[i];
-        i++;    
+        i++;
     }
     nuevo->nombre[i] = '\0';
 
@@ -28,14 +31,19 @@ Estudiante* crearEstudiante(int carnet, char nom[], float nota) {
 }
 
 // 1. Insertar un estudiante en el árbol (ordenado por carnet)
-Estudiante* insertar(Estudiante* raiz, int carnet, char nombre[], float nota) {
-    if (raiz == NULL) {
+Estudiante *insertar(Estudiante *raiz, int carnet, char nombre[], float nota)
+{
+    if (raiz == NULL)
+    {
         return crearEstudiante(carnet, nombre, nota);
     }
 
-    if (carnet < raiz->carnet) {
+    if (carnet < raiz->carnet)
+    {
         raiz->izquierdo = insertar(raiz->izquierdo, carnet, nombre, nota);
-    } else if (carnet > raiz->carnet) {
+    }
+    else if (carnet > raiz->carnet)
+    {
         raiz->derecho = insertar(raiz->derecho, carnet, nombre, nota);
     }
 
@@ -43,8 +51,10 @@ Estudiante* insertar(Estudiante* raiz, int carnet, char nombre[], float nota) {
 }
 
 // 2. Mostrar todos los estudiantes en orden de carnet.
-void mostrarEstudiantes(Estudiante* raiz) {
-    if (raiz != NULL) {
+void mostrarEstudiantes(Estudiante *raiz)
+{
+    if (raiz != NULL)
+    {
         mostrarEstudiantes(raiz->izquierdo);
         cout << "Carnet: " << raiz->carnet
              << " | Nombre: " << raiz->nombre
@@ -54,19 +64,26 @@ void mostrarEstudiantes(Estudiante* raiz) {
 }
 
 // 3. Buscar un estudiante por carnet y mostrar sus datos
-void buscarEstudiante(Estudiante* raiz, int carnet) {
-    if (raiz == NULL) {
+void buscarEstudiante(Estudiante *raiz, int carnet)
+{
+    if (raiz == NULL)
+    {
         cout << "Estudiante no encontrado.\n";
         return;
     }
 
-    if (carnet == raiz->carnet) {
+    if (carnet == raiz->carnet)
+    {
         cout << "Carnet: " << raiz->carnet
              << "\nNombre: " << raiz->nombre
              << "\nNota: " << raiz->nota << endl;
-    } else if (carnet < raiz->carnet) {
+    }
+    else if (carnet < raiz->carnet)
+    {
         buscarEstudiante(raiz->izquierdo, carnet);
-    } else {
+    }
+    else
+    {
         buscarEstudiante(raiz->derecho, carnet);
     }
 }
