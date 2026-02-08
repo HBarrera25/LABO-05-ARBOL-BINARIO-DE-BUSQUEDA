d65e89638a0dfd127c034b3c136e63c6bbc83fc7
yorchhh10 <00072325@uca.edu.sv>
2026-02-07 21:04:12 -0300

    se agrego la funcion Encontrar al estudiante con la nota más alta

--- registro_estudiantes.cpp
+++ registro_estudiantes.cpp
@@ -104,7 +104,24 @@ float calcularPromedio(Estudiante* raiz, int* contador) {
 
 // 7. Encontrar al estudiante con la nota más alta
 Estudiante* encontrarMejorNota(Estudiante* raiz) {
-    
+    Estudiante* encontrarMejorNota(Estudiante* raiz) {
+    if (raiz == NULL) {
+        return NULL;
+    }
+
+    Estudiante* mejor = raiz;
+    Estudiante* izq = encontrarMejorNota(raiz->izquierdo);
+    Estudiante* der = encontrarMejorNota(raiz->derecho);
+
+    if (izq != NULL && izq->nota > mejor->nota) {
+        mejor = izq;
+    }
+    if (der != NULL && der->nota > mejor->nota) {
+        mejor = der;
+    }
+
+    return mejor;
+}
 }
 
 int main() {
